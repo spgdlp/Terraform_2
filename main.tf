adab6d76c4a3d1937f737e2ebe5e438ddbf87976
spgdlp <spgdlp0@gmail.com>
2023-01-22 21:35:55 +0100

    añadiendo tfs

--- main.tf
+++ main.tf
@@ -26,7 +26,10 @@ provider "aws" {
 
 
 resource "aws_iam_user" "my_iam_users" {
-  count = length(var.names)
-  name =  var.names[count.index]
+  #count = length(var.names)
+  #name =  var.names[count.index]
+
+  for_each = toset(var.names)
+  name = each.value
 }
 
