e52fef55a235eaa35bb44277b4bfcfe64512e8fc
spgdlp <spgdlp0@gmail.com>
2023-01-22 19:52:46 +0100

    añadiendo tfs

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.27.0"
+    }
+  }
+}
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_iam_user" "my_iam_users" {
+  count = 2
+  name =  "my-iam-user-${count.index}"
+}
+
